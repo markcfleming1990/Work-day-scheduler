9eb3f258067bfc8314a7d095421423b8b9feeafb
Mark <markcfleming1990@gmail.com>
2022-01-08 08:57:46 -0600

    added date to top of page

--- assets/JS/JS.js
+++ assets/JS/JS.js
@@ -1,4 +1,4 @@
-//header w/ time and date
-var todayDate = moment().format('dddd, MMM, Do YYYY');
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
