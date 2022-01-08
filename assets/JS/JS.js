e83aae0078edda31ebf90c5c6008d659d3e14ff6
Mark <markcfleming1990@gmail.com>
2022-01-08 09:21:51 -0600

    added if else statements for highlight

--- assets/JS/JS.js
+++ assets/JS/JS.js
@@ -2,3 +2,35 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
+//main function
+
+//set up function
+  function timeTracker() {
+    // set up a variable this is the object that the function returns
+    var timeNow = moment().hour();
+
+    //highlight loop
+    $(".time-block").each(function () {
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+      
+      if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+      else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+
+      }
+    })
+  }
+
+  timeTracker();
